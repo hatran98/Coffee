9fe7e0de99fe4e9a4046c98b047b65c86a9a8aa4
hatran98 <getbackers0308@gmail.com>
2024-11-24 14:25:07 +0700

    ha2

--- src/database/db.js
+++ src/database/db.js
@@ -5,6 +5,9 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/price_db';
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  serverSelectionTimeoutMS: 30000,
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
